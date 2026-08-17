bbc0f950121f13bd67f2d529813cbaab38a144d6
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-08-17 19:26:20 +0600

    validate tip calculation

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const calculateTip = () => {
+  const billAmount = parseInt(document.getElementById("billAmount").value);
+  const tipAmount = parseInt(document.getElementById("tipAmount").value);
 
-  const billAmount = document.getElementById("billAmount").value;
+  if (isNaN(billAmount || isNaN(tipAmount))) {
+    document.getElementById("tipResult").innerText =
+      "Please, Enter valid number.";
 
-  const tipAmount = document.getElementById("tipAmount").value;
+    return;
+  }
+
+  const calculateTipAmount = Math.ceil((billAmount * tipAmount) / 100);
+  const totalTipAmount = billAmount + calculateTipAmount;
+
+  document.getElementById("tipResult").innerHTML = `
+  <p>Tip Amount: ${calculateTipAmount}</p>
+  <p>Tip Amount: ${totalTipAmount}</p>
+  `;
+  console.log(calculateTipAmount);
+  console.log(totalTipAmount);
 };
