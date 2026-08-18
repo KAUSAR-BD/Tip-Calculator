dd267a21195db9d6e73f311b29da40f158cbd599
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-08-18 21:15:58 +0600

    improve tip calculator calculation and output

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const calculateTip = () => {
-  const billAmount = parseInt(document.getElementById("billAmount").value);
-  const tipAmount = parseInt(document.getElementById("tipAmount").value);
+  const billAmount = parseFloat(document.getElementById("billAmount").value);
+  const tipAmount = parseFloat(document.getElementById("tipAmount").value);
 
-  if (isNaN(billAmount || isNaN(tipAmount))) {
+  if (isNaN(billAmount) || isNaN(tipAmount)) {
     document.getElementById("tipResult").innerText =
       "Please, Enter valid number.";
 
     return;
   }
 
-  const calculateTipAmount = Math.ceil((billAmount * tipAmount) / 100);
+  const calculateTipAmount = (billAmount * tipAmount) / 100;
   const totalTipAmount = billAmount + calculateTipAmount;
 
   document.getElementById("tipResult").innerHTML = `
-  <p>Tip Amount: ${calculateTipAmount}</p>
-  <p>Tip Amount: ${totalTipAmount}</p>
+  <p>Tip Amount: $${calculateTipAmount.toFixed(2)}</p>
+  <p>Tip Amount: $${totalTipAmount.toFixed(2)}</p>
   `;
-  console.log(calculateTipAmount);
-  console.log(totalTipAmount);
+  
 };
